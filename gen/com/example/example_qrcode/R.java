f5b469eb080104a46fc45b46ab846baae270d152
cike2056 <whzhn1990@163.com>
2014-12-09 13:15:11 +0800

    基本完成.但是扫描微信,一直没反应.

--- gen/com/example/example_qrcode/R.java
+++ gen/com/example/example_qrcode/R.java
@@ -10,6 +10,11 @@ package com.example.example_qrcode;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int possible_result_points=0x7f050002;
+        public static final int result_view=0x7f050000;
+        public static final int viewfinder_mask=0x7f050001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,26 +22,74 @@ public final class R {
          (such as screen margins) for screens with more than 820dp of available width. This
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
+ Default screen margins, per the Android Design guidelines. 
+
+         Example customization of dimensions originally defined in res/values/dimens.xml
+         (such as screen margins) for screens with more than 820dp of available width. This
+         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
+    
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int mm_title_back_btn=0x7f020001;
+        public static final int mm_title_back_focused=0x7f020002;
+        public static final int mm_title_back_normal=0x7f020003;
+        public static final int mm_title_back_pressed=0x7f020004;
+        public static final int mmtitle_bg_alpha=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int about_version_code=0x7f07000e;
+        public static final int action_settings=0x7f07001a;
+        /**  Messages IDs 
+         */
+        public static final int auto_focus=0x7f070000;
+        public static final int btn_create=0x7f070015;
+        public static final int btn_scanner=0x7f070016;
+        public static final int button_back=0x7f070017;
+        public static final int decode=0x7f070001;
+        public static final int decode_failed=0x7f070002;
+        public static final int decode_succeeded=0x7f070003;
+        public static final int encode_failed=0x7f070004;
+        public static final int encode_succeeded=0x7f070005;
+        public static final int et=0x7f070014;
+        public static final int gridview=0x7f07000c;
+        public static final int imgv=0x7f070013;
+        public static final int include1=0x7f070012;
+        public static final int launch_product_query=0x7f070006;
+        public static final int menu_settings=0x7f070019;
+        public static final int preview_view=0x7f070010;
+        public static final int quit=0x7f070007;
+        public static final int restart_preview=0x7f070008;
+        public static final int return_scan_result=0x7f070009;
+        public static final int search_book_contents_failed=0x7f07000a;
+        public static final int search_book_contents_succeeded=0x7f07000b;
+        public static final int split=0x7f07000f;
+        public static final int textview_title=0x7f070018;
+        public static final int viewfinder_view=0x7f070011;
+        public static final int webview=0x7f07000d;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_capture=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_title=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int activity_main=0x7f0a0000;
+        public static final int main=0x7f0a0001;
+    }
+    public static final class raw {
+        public static final int beep=0x7f040000;
+        public static final int realm_properties=0x7f040001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int action_settings=0x7f080004;
+        public static final int app_name=0x7f080000;
+        public static final int hello_world=0x7f080001;
+        public static final int menu_settings=0x7f080002;
+        public static final int scan_text=0x7f080003;
     }
     public static final class style {
         /** 
@@ -59,11 +112,33 @@ public final class R {
         res/values-v11/styles.xml on API 14+ devices.
     
  API 14 theme customizations can go here. 
+
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+
+        Base application theme for API 11+. This theme completely replaces
+        AppBaseTheme from res/values/styles.xml on API 11+ devices.
+    
+ API 11 theme customizations can go here. 
+
+        Base application theme for API 14+. This theme completely replaces
+        AppBaseTheme from BOTH res/values/styles.xml and
+        res/values-v11/styles.xml on API 14+ devices.
+    
+ API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f090000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f090001;
     }
 }
--- src/com/example/example_qrcode/MainActivity.java
+++ src/com/example/example_qrcode/MainActivity.java
@@ -1,34 +1,61 @@
 package com.example.example_qrcode;
 
+import com.qf.teach.framework.zxing.lib.activity.MipcaActivityCapture;
+import com.qf.teach.framework.zxing.lib.qrcode.util.QRCodeUtil;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
-
+	/**
+	 * 问题:imgv 1.放置生成的二维码,2.扫描到的二维码.出错.要么生成,要么扫描,只能完成一个.
+	 * 问题:扫描的窗口太小.改了activity_capture.xml中的com.qf.teach.framework.zxing.lib.qrcode.view.ViewfinderView,无效.
+	 */
+	private ImageView imgv;
+	private EditText et;
+	private QRCodeUtil util;
+	private final static int SCANNIN_GREQUEST_CODE = 1;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		imgv =(ImageView) findViewById(R.id.imgv);
+		et = (EditText) findViewById(R.id.et);
+		util = new QRCodeUtil(700, 700);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+	/**
+	 * button监听.
+	 * @param view  2014年12月9日 下午12:33:45
+	 */
+	public void btnClick(View view){
+		switch(view.getId()){
+		case R.id.btn_create:
+			String content = et.getText().toString();
+			Bitmap codeBitmap = util.createQRCodeBitmap(content);
+			imgv.setImageBitmap(codeBitmap);
+			break;
+		case R.id.btn_scanner:
+			Intent intent = new Intent(MainActivity.this,MipcaActivityCapture.class);
+			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivityForResult(intent, SCANNIN_GREQUEST_CODE);
 		}
-		return super.onOptionsItemSelected(item);
+	}
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+		if(requestCode==1&& resultCode==RESULT_OK){
+			Bundle bundle = data.getExtras();
+			et.setHint(bundle.getString("result"));
+			imgv.setImageBitmap((Bitmap) data.getParcelableExtra("bitmap"));
+		}
 	}
 }
